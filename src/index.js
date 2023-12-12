0ffe6b90d25adec4893ba8c4f713e35dbe36e22c
Snojj25 <snoj.jure1@gmail.com>
2023-12-12 07:32:16 +0100

    poseidon hash added

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ const users = require("./users/index");
 const transactions = require("./transactions/index");
 const utils = require("./utils/index");
 
-
 module.exports = { users, transactions, utils };
